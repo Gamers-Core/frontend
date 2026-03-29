b42436920890d96da95c9aa79d4ba5bea055e65f
OmarLawaty <omarlawatey@gmail.com>
2026-03-29 14:52:12 +0200

    feat: Update next.config.ts to specify allowed development origins

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['192.168.1.'],
 };
 
 const withNextIntl = createNextIntlPlugin();
