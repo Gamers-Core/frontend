5991796a2dd77cdeafa148be12b1aa0d9a9bf892
OmarLawaty <omarlawatey@gmail.com>
2026-04-14 20:22:30 +0200

    feat: Implement cartSyncMutation

--- src/hooks/cart/index.ts
+++ src/hooks/cart/index.ts
@@ -1,1 +1,2 @@
 export * from './useCartQuery';
+export * from './useCartSyncMutation';
---
+++ src/hooks/cart/useCartSyncMutation.ts
@@ -0,0 +1,22 @@
+'use client';
+
+import type { AxiosError, AxiosResponse } from 'axios';
+import { useMutation } from '@tanstack/react-query';
+
+import { BackendError, gamersCore, Cart, CreateItem, ValidationErrors } from '@/api';
+
+import { useErrorHandler } from '../useErrorHandler';
+
+export const useCartSyncMutation = () => {
+  const errorHandler = useErrorHandler();
+
+  return useMutation<Cart, BackendError<ValidationErrors> | null, CreateItem[]>({
+    mutationFn: (data) =>
+      gamersCore
+        .post<Cart, AxiosResponse<Cart>, CreateItem[]>('/cart', data)
+        .then((res) => res.data)
+        .catch((err: AxiosError<BackendError>) => {
+          throw errorHandler(err);
+        }),
+  });
+};
