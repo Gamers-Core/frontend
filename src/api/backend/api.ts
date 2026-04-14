676b322986105615797e8dbaab6087cde59a2039
OmarLawaty <omarlawatey@gmail.com>
2026-04-14 14:46:32 +0200

    refactor: Improve request header handling in gamersCore interceptor

--- src/api/backend/api.ts
+++ src/api/backend/api.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosHeaders } from 'axios';
 
 import { isClient } from '@/helpers';
 
@@ -7,25 +7,24 @@ import { getCookiesLocale, setCookiesLocale } from './helpers';
 export const gamersCore = axios.create({ baseURL: process.env.NEXT_PUBLIC_BACKEND_URL, withCredentials: true });
 
 gamersCore.interceptors.request.use(async (config) => {
-  const locale = await getCookiesLocale();
+  const headers = AxiosHeaders.from(config.headers);
 
-  if (locale) config.headers['x-locale'] = locale;
+  const locale = await getCookiesLocale();
+  if (locale) headers.set('x-locale', locale);
 
   if (!isClient()) {
     const { cookies } = await import('next/headers');
     const cookieStore = await cookies();
-
     const cookieHeader = cookieStore
       .getAll()
       .map((c) => `${c.name}=${c.value}`)
       .join('; ');
-
-    config.headers.set('Cookie', cookieHeader);
+    headers.set('Cookie', cookieHeader);
   }
 
+  config.headers = headers;
   return config;
 });
-
 gamersCore.interceptors.response.use((res) => {
   const newLocale = res.headers['x-locale'];
 
