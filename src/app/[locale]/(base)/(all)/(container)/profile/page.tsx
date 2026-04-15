95838b2d1ae3a10b118b8bd4dba768b013c751d0
OmarLawaty <omarlawatey@gmail.com>
2026-04-15 16:18:52 +0200

    feat: Create layout and profile page components for user profile management

---
+++ src/app/[locale]/(base)/(all)/(container)/profile/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Gamers Core | Profile',
+  description: 'Access and manage your Gamers Core profile, view your order history, and update your account settings.',
+};
+
+export default async function Page() {
+  return <></>;
+}
---
+++ src/app/[locale]/(base)/(all)/(non-container)/layout.tsx
@@ -0,0 +1,3 @@
+export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return <main className="flex-1 w-full py-14 flex flex-col gap-4">{children}</main>;
+}
