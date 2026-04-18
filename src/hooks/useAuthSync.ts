98725aedbd737ce025d7aebd567ebf5f88d8e102
OmarLawaty <omarlawatey@gmail.com>
2026-04-18 23:58:32 +0200

    feat: Add unauthorized session error message and handle logout in useAuthSync hook

--- src/hooks/useAuthSync.ts
+++ src/hooks/useAuthSync.ts
@@ -1,16 +1,33 @@
 'use client';
 
 import { useEffect } from 'react';
+import { toast } from 'sonner';
+import { useTranslations } from 'next-intl';
 
 import { useAuthStore } from '@/stores';
 
 import { useMeQuery } from './users';
+import { useLogoutMutation } from './auth';
 
 export const useAuthSync = () => {
+  const t = useTranslations();
+
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
   const setUser = useAuthStore((state) => state.setUser);
 
   const meQuery = useMeQuery(false, isLoggedIn);
+  const logoutMutation = useLogoutMutation();
+
+  useEffect(() => {
+    if (meQuery.error?.response?.data.status === 401)
+      logoutMutation.mutate(void 0, {
+        onSuccess: () => {
+          toast.error(t('error_unauthorized'));
+        },
+      });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [meQuery.error]);
 
   useEffect(() => {
     if (!meQuery.isSuccess) return setUser(null);
