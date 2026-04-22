a7e27bb4b34435239ec429ed4ef378c63b671231
OmarLawaty <omarlawatey@gmail.com>
2026-04-23 01:34:31 +0200

    feat: Update padding in Layout component for improved spacing

--- src/app/[locale]/(hydration)/(base)/(all)/(non-container)/layout.tsx
+++ src/app/[locale]/(hydration)/(base)/(all)/(non-container)/layout.tsx
@@ -1,3 +1,3 @@
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <main className="flex-1 w-full pt-14 pb-8 flex flex-col gap-4">{children}</main>;
+  return <main className="flex-1 w-full py-14 lg:pt-20 flex flex-col gap-4">{children}</main>;
 }
