aff61e58cd2c7797c109cfa9803cace4fbb292ce
OmarLawaty <omarlawatey@gmail.com>
2026-04-24 12:13:11 +0300

    feat: Update useDebounce hook to serialize object values for improved debouncing accuracy

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,15 +1,18 @@
 'use client';
-
 import { useEffect, useState } from 'react';
 
 export const useDebounce = <T>(value: T, delay: number = 700): T => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
+  const serialized =
+    typeof value === 'object' && value !== null ? JSON.stringify(value, Object.keys(value).sort()) : value;
+
   useEffect(() => {
     const handler = setTimeout(() => setDebouncedValue(value), delay);
 
     return () => clearTimeout(handler);
-  }, [value, delay]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [serialized, delay]);
 
   return debouncedValue;
 };
