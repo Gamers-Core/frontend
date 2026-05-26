828e75c9f01174095d1cfcd7c2ae00323b299070
OmarLawaty <omarlawatey@gmail.com>
2026-05-26 17:22:31 +0300

    feat: Add @tailwindcss/typography package and integrate it into globals.css; update HTMLRender component for className support

--- src/components/HTMLRender.tsx
+++ src/components/HTMLRender.tsx
@@ -1,13 +1,19 @@
 import { memo } from 'react';
 
+import { cn } from '@/lib/utils';
+
 interface HTMLRenderProps {
   html: string;
   className?: string;
 }
 
 export const HTMLRender = (displayName: string) => {
-  const Component = memo(({ html, ...props }: HTMLRenderProps) => (
-    <div dangerouslySetInnerHTML={{ __html: html }} {...props} />
+  const Component = memo(({ html, className, ...props }: HTMLRenderProps) => (
+    <div
+      dangerouslySetInnerHTML={{ __html: html }}
+      className={cn('prose prose-sm md:prose-base dark:prose-invert max-w-none prose-li:leading-normal', className)}
+      {...props}
+    />
   ));
 
   Component.displayName = displayName;
